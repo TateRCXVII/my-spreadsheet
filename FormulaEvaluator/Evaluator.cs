059a0e855051715211cf731cd0ba6765eb41511e
TateRCXVII <tate.reynolds@gmail.com>
2022-01-13 13:47:15 -0700

    Added header comments

---
+++ FormulaEvaluator/Evaluator.cs
@@ -0,0 +1,21 @@
+﻿namespace FormulaEvaluator
+{
+    /// <summary> 
+    /// Author:    Tate Reynolds 
+    /// Partner:   None 
+    /// Date:      1/13/22 
+    /// Course:    CS 3500, University of Utah, School of Computing 
+    /// Copyright: CS 3500 and Tate Reynolds - This work may not be copied for use in Academic Coursework. 
+    /// 
+    /// I, Tate Reynolds, certify that I wrote this code from scratch and did not copy it in part or whole from  
+    /// another source.  All references used in the completion of the assignment are cited in my README file. 
+    /// 
+    /// File Contents 
+    /// 
+    ///    [... and of course you should describe the contents of the file in broad terms here ...] 
+    /// </summary>
+    public class Evaluator
+    {
+
+    }
+}
