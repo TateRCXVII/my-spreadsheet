db818324735a16cbce8bd55b18f22c4e88c82d80
TateRCXVII <tate.reynolds@gmail.com>
2022-01-18 21:47:45 -0700

    Added simple test for variables. More testing to come

--- Test_The_Evaluator_Console_App/Program.cs
+++ Test_The_Evaluator_Console_App/Program.cs
@@ -8,3 +8,12 @@ if (Evaluator.Evaluate("20 /5", null) == 4) Console.WriteLine("Happy Day!20 /5 w
 if (Evaluator.Evaluate("(25/5)+3", null) == 8) Console.WriteLine("Happy Day!(25/5)+3 works");
 if (Evaluator.Evaluate("((25/5)+3)*5", null) == 40) Console.WriteLine("Happy Day!((25/5)+3)*5 works");
 if (Evaluator.Evaluate("(5+3) * 8 - 100", null) == -36) Console.WriteLine("Happy Day!(5+3) * 8 - 100 works");
+if (Evaluator.Evaluate("(5+3) * 8 - ABCD1", VariableEvaluator) == -36) Console.WriteLine("Happy Day!(5+3) * 8 - ABCD1 works");
+
+static int VariableEvaluator(string s)
+{
+    if (s.Equals(" ABCD1"))
+        return 100;
+    else
+        return 50;
+}
