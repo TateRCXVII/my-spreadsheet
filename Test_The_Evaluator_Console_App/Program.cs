0ad0aa148c8ded7123c43b4975f2036c0b2050c6
TateRCXVII <tate.reynolds@gmail.com>
2022-01-20 21:05:07 -0700

    Added test method to reduce reduntant code. More tests needed!

--- Test_The_Evaluator_Console_App/Program.cs
+++ Test_The_Evaluator_Console_App/Program.cs
@@ -1,32 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using FormulaEvaluator;
 
-Console.WriteLine(Evaluator.Evaluate("(120/((5+3) * 8)) - 100 * 2", null));
+//Console.WriteLine(Evaluator.Evaluate("(120/((5+3) * 8)) - 100 * 2", null));
 if (Evaluator.Evaluate("25/5", null) == 5) Console.WriteLine("Happy Day!25/5 works");
 if (Evaluator.Evaluate("5*5", null) == 25) Console.WriteLine("Happy Day!5*5 works");
 if (Evaluator.Evaluate("20 /5", null) == 4) Console.WriteLine("Happy Day!20 /5 works");
 if (Evaluator.Evaluate("(25/5)+3", null) == 8) Console.WriteLine("Happy Day!(25/5)+3 works");
 if (Evaluator.Evaluate("((25/5)+3)*5", null) == 40) Console.WriteLine("Happy Day!((25/5)+3)*5 works");
 if (Evaluator.Evaluate("(5+3) * 8 - 100", null) == -36) Console.WriteLine("Happy Day!(5+3) * 8 - 100 works");
-if (Evaluator.Evaluate("(120/((5+3) * 8)) - 100 * B8923", VariableEvaluator) == -200) Console.WriteLine("Happy Day!(120/((5+3) * 8)) - 100 * B8923 works");
+if (Evaluator.Evaluate("(120/((5+3) * 8)) - 100 * B8923", VariableEvaluator) == -199) Console.WriteLine("Happy Day!(120/((5+3) * 8)) - 100 * B8923 works");
 if (Evaluator.Evaluate("(5+3) * 8 - ABCD1", VariableEvaluator) == -36) Console.WriteLine("Happy Day!(5+3) * 8 - ABCD1 works");
+
+
+TestEvaluate("(120/((5+3) * 8)) - 100 * B8923", VariableEvaluator, -199, "complex arithmatic with a variable:");
+TestEvaluate("((((5+5))))", null, 10, "lots of parentheses:");
+TestEvaluate("(10*10)/0", null, 0, "divide by zero exception test: ");
+TestEvaluate("(10*10)/charlie1", VariableEvaluator, 0, "divide by zero with a variable exception test: ");
+
 
 static int VariableEvaluator(string s)
 {
-    if (s.Equals(" ABCD1"))
+    if (s.Equals("ABCD1"))
         return 100;
-    else if (s.Equals(" B8923"))
+    else if (s.Equals("B8923"))
         return 2;
+    else if (s.Equals("charlie1"))
+        return 0;
     else return 0;
 }
 
-static void TestEvaluate()
+void TestEvaluate(string expression, Evaluator.Lookup var, int expected, string description)
 {
     try
     {
-
+       int actual = Evaluator.Evaluate(expression, var);
+        if (actual != expected)
+            Console.WriteLine(description + " " + expression + " error.");
     } catch(ArgumentException e)
     {
-        Console.WriteLine(e.Message + "Syntax error or divide by 0");
+        Console.WriteLine(e.Message + " Invalid input or divide by 0");
     }
 }
