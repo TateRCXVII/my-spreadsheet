624ee56e1f0be1b2aadb4a97cfbaeae369c13a7c
TateRCXVII <tate.reynolds@gmail.com>
2022-01-21 19:45:16 -0700

    Added more tests for exceptions and other errors.

--- Test_The_Evaluator_Console_App/Program.cs
+++ Test_The_Evaluator_Console_App/Program.cs
@@ -2,20 +2,30 @@
 using FormulaEvaluator;
 
 //Console.WriteLine(Evaluator.Evaluate("(120/((5+3) * 8)) - 100 * 2", null));
-if (Evaluator.Evaluate("25/5", null) == 5) Console.WriteLine("Happy Day!25/5 works");
+/*if (Evaluator.Evaluate("25/5", null) == 5) Console.WriteLine("Happy Day!25/5 works");
 if (Evaluator.Evaluate("5*5", null) == 25) Console.WriteLine("Happy Day!5*5 works");
 if (Evaluator.Evaluate("20 /5", null) == 4) Console.WriteLine("Happy Day!20 /5 works");
 if (Evaluator.Evaluate("(25/5)+3", null) == 8) Console.WriteLine("Happy Day!(25/5)+3 works");
 if (Evaluator.Evaluate("((25/5)+3)*5", null) == 40) Console.WriteLine("Happy Day!((25/5)+3)*5 works");
 if (Evaluator.Evaluate("(5+3) * 8 - 100", null) == -36) Console.WriteLine("Happy Day!(5+3) * 8 - 100 works");
 if (Evaluator.Evaluate("(120/((5+3) * 8)) - 100 * B8923", VariableEvaluator) == -199) Console.WriteLine("Happy Day!(120/((5+3) * 8)) - 100 * B8923 works");
-if (Evaluator.Evaluate("(5+3) * 8 - ABCD1", VariableEvaluator) == -36) Console.WriteLine("Happy Day!(5+3) * 8 - ABCD1 works");
+if (Evaluator.Evaluate("(5+3) * 8 - ABCD1", VariableEvaluator) == -36) Console.WriteLine("Happy Day!(5+3) * 8 - ABCD1 works");*/
 
 
 TestEvaluate("(120/((5+3) * 8)) - 100 * B8923", VariableEvaluator, -199, "complex arithmatic with a variable:");
 TestEvaluate("((((5+5))))", null, 10, "lots of parentheses:");
-TestEvaluate("(10*10)/0", null, 0, "divide by zero exception test: ");
-TestEvaluate("(10*10)/charlie1", VariableEvaluator, 0, "divide by zero with a variable exception test: ");
+TestEvaluateException("(10*10)/0", null, 0, "divide by zero exception test: ");
+TestEvaluateException("(10*10)/charlie1", VariableEvaluator, 0, "divide by zero with a variable exception test: ");
+TestEvaluate("((3))", null, 3, "single integer performance: ");
+TestEvaluateException(null, null, 0, "Null expression error test: ");
+TestEvaluateException("3+3)/200", null, 0, "Invalid input, incorrect parenthesis: ");
+TestEvaluateException(" 3 + -5", null, 2, "Negative number exception test: ");
+TestEvaluateException("8*(-3)", null, 0, "Negative multiplication test: ");
+TestEvaluate("()5", null, 5, "Random parentheses test: ");
+TestEvaluateException("5/1c4f", VariableEvaluator, 0, "Bad variable exception test: ");
+TestEvaluate("100-50+ABCD1", VariableEvaluator, 150, "Complex equation with no spaces: ");
+TestEvaluate("1(1)++1", VariableEvaluator, 3, "Piazza post test: ");
+//(35/7)*
 
 
 static int VariableEvaluator(string s)
@@ -26,7 +36,7 @@ static int VariableEvaluator(string s)
         return 2;
     else if (s.Equals("charlie1"))
         return 0;
-    else return 0;
+    else throw new ArgumentException("Invalid variable lookup.");
 }
 
 void TestEvaluate(string expression, Evaluator.Lookup var, int expected, string description)
@@ -38,6 +48,19 @@ void TestEvaluate(string expression, Evaluator.Lookup var, int expected, string 
             Console.WriteLine(description + " " + expression + " error.");
     } catch(ArgumentException e)
     {
-        Console.WriteLine(e.Message + " Invalid input or divide by 0");
+       Console.WriteLine(e.Message + " Invalid input or divide by 0");
+    }
+}
+
+void TestEvaluateException(string expression, Evaluator.Lookup var, int expected, string description)
+{
+    try
+    {
+        int actual = Evaluator.Evaluate(expression, var);
+        Console.WriteLine(description + " " + expression + " error.");
+    }
+    catch (ArgumentException e)
+    {
+        //Console.WriteLine(e.Message + description);
     }
 }
