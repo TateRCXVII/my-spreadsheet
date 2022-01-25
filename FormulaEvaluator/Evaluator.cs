87fda8035d7d60b712139a23523c1c71c5ba038c
TateRCXVII <tate.reynolds@gmail.com>
2022-01-25 15:06:31 -0700

    added a simple todo. More changes needed

--- FormulaEvaluator/Evaluator.cs
+++ FormulaEvaluator/Evaluator.cs
@@ -59,6 +59,7 @@ namespace FormulaEvaluator
             {
                 if (token.Equals(" ") || token.Equals("")) continue;
 
+                //TODO: resolve trim
                 string eqnPart = token.Trim();
 
                 int val = 0;
