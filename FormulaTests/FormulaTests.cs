137907c234978a672b576fcd41083a1b32ecb643
TateRCXVII <tate.reynolds@gmail.com>
2022-02-01 13:39:16 -0700

    Added a few tests for equality on TDD

--- FormulaTests/FormulaTests.cs
+++ FormulaTests/FormulaTests.cs
@@ -1,13 +1,50 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpreadsheetUtilities;
 
 namespace FormulaTests
 {
     [TestClass]
     public class FormulaTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        // ************************** TESTS ON EQUALITY ************************* //
+
+        /// <summary>
+        ///Null formula shouldn't equal non-null formula
+        ///</summary>
+        [TestMethod(), Timeout(2000)]
+        [TestCategory("Equality")]
+        public void NullNotEqualNonNullTest()
         {
+            Formula? empty = null;
+            Formula notEmpty = new Formula("40+30.5*100");
+            Assert.IsFalse(empty == notEmpty);
+            Assert.IsFalse(empty.Equals(notEmpty));
+        }
+
+        /// <summary>
+        ///Null formula should equal other null formula
+        ///</summary>
+        [TestMethod(), Timeout(2000)]
+        [TestCategory("Equality")]
+        public void NullEqualNullTest()
+        {
+            Formula? empty = null;
+            Formula? notEmpty = null;
+            Assert.IsTrue(empty == notEmpty);
+            Assert.IsTrue(empty.Equals(notEmpty));
+        }
+
+        /// <summary>
+        ///Normalized Formulas should equal eachother
+        ///</summary>
+        [TestMethod(), Timeout(2000)]
+        [TestCategory("Equality")]
+        public void NormalizedEqualityTest()
+        {
+            Formula? form1 = new Formula("40+30.5*100/Xy1"); ;
+            Formula form2 = new Formula("40 + 30.5 * 100 / xY1");
+            Assert.IsTrue(form1 == form2);
+            Assert.IsTrue(form1.Equals(form2));
         }
     }
 }
