99cac31c216c90c1c790983d07aa5ccf60845248
TateRCXVII <tate.reynolds@gmail.com>
2022-02-06 16:49:36 -0700

    Created a class to represent cells.

---
+++ Spreadsheet/Cell.cs
@@ -0,0 +1,64 @@
+﻿using SpreadsheetUtilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spreadsheet
+{
+    internal class Cell
+    {
+        private string _name;
+
+        //String, double (including result from Formula.evaluate), or formula error
+        //displayed on screen without selection
+        private object _value;
+
+        //String, double, or formula
+        //empty string "" means empty cell
+        //Displayed when the cell is selected (double clicked)
+        //new SS = ""
+        private object _contents;
+
+        /// <summary>
+        /// Creates an empty cell
+        /// </summary>
+        public Cell(string name)
+        {
+            _name = name;
+            _value = "";
+            _contents = "";
+        }
+
+        /// <summary>
+        /// Creates a cell with formula as contents and its result as its value.
+        /// </summary>
+        public Cell(string name, Formula formula)
+        {
+            _name = name;
+            _value = formula.Evaluate;
+            _contents = formula;
+        }
+
+        /// <summary>
+        /// Creates a cell with a number
+        /// </summary>
+        public Cell(string name, double number)
+        {
+            _name = name;
+            _value = number;
+            _contents = number;
+        }
+
+        /// <summary>
+        /// Creates a cell with text
+        /// </summary>
+        public Cell(string name, string text)
+        {
+            _name = name;
+            _value = text;
+            _contents = text;
+        }
+    }
+}
