9d267ad00b3d63bc1df7c2b63de67592d190d0b9
TateRCXVII <tate.reynolds@gmail.com>
2022-02-08 15:16:33 -0700

    Added some tests to test unimplemented methods and their simple functions

--- SpreadsheetTests/SpreadsheetTests.cs
+++ SpreadsheetTests/SpreadsheetTests.cs
@@ -1,32 +1,57 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpreadsheetUtilities;
 using SS;
+using System.Collections.Generic;
 
 namespace SpreadsheetTests
 {
     [TestClass]
     public class SpreadsheetTests
     {
+        /// <summary>
+        /// See title
+        /// </summary>
         [TestMethod]
         [Timeout(5000)]
         [ExpectedException(typeof(CircularException))]
         public void CircularExceptionTest()
         {
-
+            AbstractSpreadsheet spreadsheet = new SS.Spreadsheet();
+            Formula formula = new Formula("A1+B1");
+            spreadsheet.SetCellContents("A1", formula);
         }
 
+        /// <summary>
+        /// See name
+        /// </summary>
         [TestMethod]
         [Timeout(5000)]
         [ExpectedException(typeof(InvalidNameException))]
         public void InvalidNameExceptionTest()
         {
-
+            AbstractSpreadsheet spreadsheet = new SS.Spreadsheet();
+            spreadsheet.GetCellContents("3g4");
         }
 
         [TestMethod]
         [Timeout(5000)]
-        public void GetNamesTest()
+        public void GetNamesSimpleTest()
         {
-
+            SS.Spreadsheet spreadsheet = new SS.Spreadsheet();
+            spreadsheet.SetCellContents("A1", 30);
+            spreadsheet.SetCellContents("B1", 30);
+            spreadsheet.SetCellContents("C1", 30);
+            spreadsheet.SetCellContents("D1", 30);
+            List<string> expected = new List<string>();
+            expected.Add("A1");
+            expected.Add("B1");
+            expected.Add("C1");
+            expected.Add("D1");
+            IEnumerable<string> cellNames = spreadsheet.GetNamesOfAllNonemptyCells();
+            foreach (string name in cellNames)
+            {
+                Assert.IsTrue(expected.Contains(name));
+            }
         }
 
 
